1111b94ef7137b01df079506e2b3a7f12888e80d
Dmitry Fomin <fomin.dm@gmail.com>
2018-10-03 20:30:09 +0300

    Echo bot.

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+from telegram.ext import Updater, Filters, MessageHandler
+
+token = ''
+
+
+def echo(bot, update):
+    update.message.reply_text(update.message.text)
+
+
 def main():
-    pass
+    updater = Updater(token)
+    updater.start_webhook(listen='127.0.0.1', port=5000, url_path=token)
+    updater.bot.set_webhook(webhook_url='https://pigowl.com/' + token, certificate=open('fullchain.pem', 'rb'))
+
+    dp = updater.dispatcher
+
+    dp.add_handler(MessageHandler(Filters.text, echo))
+
+    updater.idle()
+
 
 if __name__ == "__main__":
     main()
